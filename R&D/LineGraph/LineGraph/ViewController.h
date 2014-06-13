332f73d4abe9037a5f99ce7d6b62d43ac183fcc2
Avinash <avinashkiran001@gmail.com>
2014-06-13 18:02:19 +0530

    21
    23

---
+++ R&D/LineGraph/LineGraph/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  LineGraph
+//
+//  Created by Flatmind on 6/13/14.
+//  Copyright (c) 2014 avanitechnologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface ViewController : UIViewController
+
+@end
---
+++ R&D/LineGraph/LineGraph/ViewController.m
@@ -0,0 +1,53 @@
+//
+//  ViewController.m
+//  LineGraph
+//
+//  Created by Flatmind on 6/13/14.
+//  Copyright (c) 2014 avanitechnologies. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "AKLineChartView.h"
+
+@interface ViewController ()
+
+@property (nonatomic, strong) AKLineChartView *lineChartView;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    [self setUpGraph];
+}
+
+
+-(void)setUpGraph
+{
+    _lineChartView = [[AKLineChartView alloc] initWithFrame:CGRectMake(10,100,[self.view bounds].size.width-20,[self.view bounds].size.width-20)];
+    [_lineChartView setAutoresizingMask:UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight];
+    [self.view addSubview:_lineChartView];
+    
+    AKLineComponent *component = [[AKLineComponent alloc] init];
+    [component setTitle:@""];
+    [component setPoints:@[[NSNumber numberWithInt:10],[NSNumber numberWithInt:50],[NSNumber numberWithInt:20],[NSNumber numberWithInt:150],[NSNumber numberWithInt:105],[NSNumber numberWithInt:80],[NSNumber numberWithInt:10],[NSNumber numberWithInt:50],[NSNumber numberWithInt:20],[NSNumber numberWithInt:150],[NSNumber numberWithInt:105],[NSNumber numberWithInt:80]]];
+    [component setShouldLabelValues:YES];
+    [component setColour:[UIColor whiteColor]];
+    [_lineChartView setComponents:(NSMutableArray *)@[component]];
+    [_lineChartView setXLabels:(NSMutableArray *)@[@"JAN",@"FEB",@"MAR",@"APR",@"MAY",@"JUNE",@"JUL",@"AUG",@"SEP",@"OCT",@"NOV",@"DEC"]];
+
+
+
+    
+
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
